d9c0fef7677e593d012653d08ad688291039dd0c
Dan <danko56666@gmail.com>
2020-05-10 02:03:55 -0400

    Updates

---
+++ precache-manifest.d651488b7707def283e8dfbeba33b28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76398c8f74a6979242e8fbd9aaa070b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "afa011475769dd8d9494",
+    "url": "/monsters-rolodex/static/css/main.4b66cd88.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa011475769dd8d9494",
+    "url": "/monsters-rolodex/static/js/main.7d78af8f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
